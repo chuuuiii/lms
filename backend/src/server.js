efe1fb83ccd67c81f61145f25ada655c8e553873
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-12-19 21:14:32 +0800

    user routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import morgan from 'morgan';
 import { connectDB } from './config/db.js';
 import { config } from './config/env.js';
+import userRoutes from './routes/user.routes.js';
 
 
 const app = express();
@@ -14,6 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'))
+
+//routes
+app.use('/api/users', userRoutes)
 
 app.get('/', (req, res) => {
   res.send('Server ready')
