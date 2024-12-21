faf9af38a717666fb64e8590b6871cd81b40ad59
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-12-21 19:56:54 +0800

    fixed email and password

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,6 +1,9 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/user.model.js'
 import { createError } from '../utils/errorUtils.js';
+import { generateToken } from '../utils/jwt.js';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
 
 export const createUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -10,18 +13,26 @@ export const createUser = asyncHandler(async (req, res) => {
     throw createError(400, 'Please provide all the required fields');
   }
 
+  if (!validator.isEmail(email)) {
+    throw createError(400, 'Please provide a valid email');
+  }
+
   const userExists = await User.findOne({ email });
   if (userExists) {
     // res.status(400);
     throw createError(400, 'User already registered');
   }
 
-  const user = await User.create({ name, email, password, membershipStatus: 'active' });
-  if (user) {
-    res.status(201).json({ success: true, message: 'User created successfully', user: { _id: user._id, name: user.name, email: user.email, membershipStatus: user.membershipStatus }});
-  } 
-  // else {
-  //   res.status(400);
-  //   throw new Error('Invalid user data');
-  // }
+  if (password.length < 6) {
+    throw createError(400, 'Password must be at least 6 characters long');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+  
+  const user = await User.create({ name, email, password: hashedPassword, membershipStatus: 'Active', });
+  const token = generateToken(user._id);
+  res.status(201).json({ success: true, message: 'User created successfully', user: {
+    _id: user._id, name: user.name, email: user.email, role: user.role, membershipStatus: user.membershipStatus, token: token
+  }});
+
 });
