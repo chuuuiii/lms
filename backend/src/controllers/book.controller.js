d065501bb166e54170ee17c21b145e3109303a1e
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-12-22 21:15:48 +0800

    Update and delete books fixed and validation

--- backend/src/controllers/book.controller.js
+++ backend/src/controllers/book.controller.js
@@ -1,27 +1,39 @@
 import Book from "../models/book.model.js";
 import AsyncHandler from "express-async-handler";
 import { createError } from "../utils/errorUtils.js";
+import mongoose from "mongoose";
 
+//get all books
+export const getAllBooks = AsyncHandler (async (req, res) => {
 
+  const books = await Book.find({}).sort({ createdAt: -1 });
 
+  res.status(200).json(books);
+
+});
+
+//create book
 export const createBook = AsyncHandler(async (req, res) => {
   const { title, author, genre, ISBN, totalCopies } = req.body;
 
   if (!title || !author || !genre || !ISBN || !totalCopies) {
+    res.status(400)
     throw createError(400, 'Please provide all the required fields');
   }
 
   const bookExists = await Book.findOne({ ISBN});
   if (bookExists) {
+    res.status(400);
     throw createError(400, 'Book already registered with this ISBN');
   }
 
   const book = await Book.create({ title, author, genre, ISBN, totalCopies});
 
-  res.status(200).json(book);
+  res.status(200).json({ success: true, message: 'Book created successfully', book });
 
 });
 
+//check book availability
 export const checkBookAvailability = AsyncHandler(async (req, res) => {
   // const { id } = req.params;
 
@@ -30,8 +42,46 @@ export const checkBookAvailability = AsyncHandler(async (req, res) => {
   const book = await Book.findById(req.params.id);
 
   if (!book) {
+    res.status(404);
     throw createError(404, 'Book not fount');
   }
 
   res.status(200).json({ availability: book.availability, totalCopies: book.totalCopies, currentBorrows: book.currentBorrows })
+});
+
+//update books
+export const updateBookDetails = AsyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400);
+    throw createError(400, 'Invalid book ID');
+  }
+
+  const updateBook = await Book.findByIdAndUpdate(id, req.body, { new: true });
+  if (!updateBook) {
+    res.status(404);
+    throw createError(404, 'Book not found');
+  }
+
+  res.status(200).json({ success: true, message: 'Book details updated successfully', updateBook });
+});
+
+//delete book
+export const deleteBook = AsyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400);
+    throw createError(400, 'Invalid book ID');
+  }
+
+  const deleteBook = await Book.findByIdAndDelete(id);
+  if (!deleteBook) {
+    res.status(404);
+    throw createError(404, 'Book not found')
+  }
+
+  res.status(200).json({ success: true, message: 'Book deleted successfully', deleteBook})
+
 });
--- backend/src/routes/book.routes.js
+++ backend/src/routes/book.routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { createBook, checkBookAvailability } from '../controllers/book.controller.js';
+import { createBook, checkBookAvailability, updateBookDetails, getAllBooks, deleteBook } from '../controllers/book.controller.js';
 
 const router = express.Router();
 
+router.get('/', getAllBooks);
 router.post('/', createBook);
 router.get('/availability/:id', checkBookAvailability);
+router.put('/:id', updateBookDetails);
+router.delete('/:id', deleteBook);
 
 export default router;
